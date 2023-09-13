b08c1787ed3d5cefd60cec5eeb6b15ea5ea33e47
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-13 14:06:45 -0300

    Update Filtros.vue

--- src/components/Filtros.vue
+++ src/components/Filtros.vue
@@ -5,11 +5,8 @@
 
 <template>
     <div class="filtros sombra contenedor">
-
         <form>
-
             <div class="campo">
-
                 <label for="filtro">Filtrar Gastos</label>
                 
                 <select id="filtro" :value="filtro" @input="$emit('update:filtro', $event.target.value)">
@@ -22,34 +19,31 @@
                         <option value="salud">Salud</option>
                         <option value="suscripciones">Suscripciones</option>
                 </select>
-
             </div>
-
         </form>
-
     </div>
 </template>
 
 <style scoped>
-    .filtros {
-        margin-top: 10rem;
-    }
-    .filtros .campo {
-        display: flex;
-        align-items: center;
-        gap: 2rem;
-    }
-    .filtros label {
-        font-size: 3rem;
-        font-weight: 900;
-        color: var(--gris-oscuro);
-    }
-    .filtros select {
-        flex: 1;
-        padding: 1rem;
-        border: none;
-        border-radius: 1rem;
-        background-color: var(--gris-claro);
-        text-align: center;
-    }
+.filtros {
+    margin-top: 10rem;
+}
+.filtros .campo {
+    display: flex;
+    align-items: center;
+    gap: 2rem;
+}
+.filtros label {
+    font-size: 3rem;
+    font-weight: 900;
+    color: var(--gris-oscuro);
+}
+.filtros select {
+    flex: 1;
+    padding: 1rem;
+    border: none;
+    border-radius: 1rem;
+    background-color: var(--gris-claro);
+    text-align: center;
+}
 </style>
